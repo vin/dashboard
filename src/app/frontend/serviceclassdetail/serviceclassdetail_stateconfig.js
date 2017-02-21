d1a1d4f05ae84f61d63075ec270c26ff881f4a35
Ross Holland <rossholland@rossholland.sea.corp.google.com>
2017-02-20 18:48:36 -0800

    Got the serviceclassdetail page to use real data

--- src/app/frontend/serviceclassdetail/serviceclassdetail_stateconfig.js
+++ src/app/frontend/serviceclassdetail/serviceclassdetail_stateconfig.js
@@ -12,79 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * @param {string} name
- * @return {?}
- */
-function getServiceClassDetailFakeData(name) {
-  const serviceClassDetailFakeData = {
-    'booksbe': {
-      'Bindable': true,
-      'BrokerName': 'k8s',
-      'OSBDashboardOAuth2ClientID': '',
-      'OSBDashboardRedirectURI': '',
-      'OSBDashboardSecret': '',
-      'OSBGUID': '5F21BAA6-CF52-43DF-8797-3A7D3C1F5629',
-      'OSBMaxDBPerNode': '',
-      'OSBMetadata': null,
-      'OSBRequires': null,
-      'OSBTags': null,
-      'PlanUpdatable': false,
-      'Plans': [{
-        'Name': 'default',
-        'OSBFree': false,
-        'OSBGUID': '0CEC1E1F-CAFA-40C7-9761-491D0B164204',
-        'OSBMetadata': null,
-      }],
-      'apiVersion': 'catalog.k8s.io/v1alpha1',
-      'creationTimestamp': null,
-      'kind': 'ServiceClass',
-      'metadata': {
-        'name': 'booksbe',
-        'namespace': 'default',
-        'selfLink': '/apis/catalog.k8s.io/v1alpha1/namespaces/default/serviceclasses/booksbe',
-        'uid': 'c4bd388c-e415-11e6-bf38-42010a8a0138',
-        'resourceVersion': '17097',
-        'creationTimestamp': '2017-01-26T22:21:22Z',
-      },
-      'name': 'booksbe',
-    },
-    'user-provided-service': {
-      'Bindable': false,
-      'BrokerName': 'ups',
-      'OSBDashboardOAuth2ClientID': '',
-      'OSBDashboardRedirectURI': '',
-      'OSBDashboardSecret': '',
-      'OSBGUID': '4F6E6CF6-FFDD-425F-A2C7-3C9258AD2468',
-      'OSBMaxDBPerNode': '',
-      'OSBMetadata': null,
-      'OSBRequires': null,
-      'OSBTags': null,
-      'PlanUpdatable': false,
-      'Plans': [{
-        'Name': 'default',
-        'OSBFree': true,
-        'OSBGUID': '86064792-7ea2-467b-af93-ac9694d96d52',
-        'OSBMetadata': null,
-      }],
-      'apiVersion': 'catalog.k8s.io/v1alpha1',
-      'creationTimestamp': null,
-      'kind': 'ServiceClass',
-      'metadata': {
-        'name': 'user-provided-service',
-        'namespace': 'default',
-        'selfLink':
-            '/apis/catalog.k8s.io/v1alpha1/namespaces/default/serviceclasses/user-provided-service',
-        'uid': 'c4bec2e8-e415-11e6-bf38-42010a8a0138',
-        'resourceVersion': '17098',
-        'creationTimestamp': '2017-01-26T22:21:22Z',
-      },
-      'name': 'user-provided-service',
-    },
-  };
-  return serviceClassDetailFakeData[name];
-}
-
 import {stateName as chromeStateName} from 'chrome/chrome_state';
 import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_service';
 import {appendDetailParamsToUrl} from 'common/resource/resourcedetail';
@@ -104,6 +31,7 @@ export default function stateConfig($stateProvider) {
     url: appendDetailParamsToUrl(stateUrl),
     parent: chromeStateName,
     resolve: {
+      'serviceClassDetailResource': getServiceClassDetailResource,
       'serviceClass': resolveServiceClassDetail,
     },
     data: {
@@ -122,10 +50,23 @@ export default function stateConfig($stateProvider) {
   });
 }
 
+
 /**
- * @return {?}
+ * @param {!./../common/resource/resourcedetail.StateParams} $stateParams
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource<!backendApi.ServiceDetail>}
  * @ngInject
  */
-export function resolveServiceClassDetail($stateParams) {
-  return Promise.resolve(getServiceClassDetailFakeData($stateParams.objectName));
+export function getServiceClassDetailResource($stateParams, $resource) {
+  return $resource(`api/v1alpha1/serviceclass/${$stateParams.objectName}`);
+}
+
+
+/**
+ * @param {!angular.Resource} serviceClassDetailResource
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function resolveServiceClassDetail(serviceClassDetailResource) {
+  return serviceClassDetailResource.get().$promise;
 }
