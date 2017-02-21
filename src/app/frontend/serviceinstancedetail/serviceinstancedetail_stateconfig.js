73c21376420ee220cfaf69b43b23f73cc70b9ea2
Ross Holland <rossholland@rossholland.sea.corp.google.com>
2017-02-20 19:38:05 -0800

    got the servince instance detail page to use real data

--- src/app/frontend/serviceinstancedetail/serviceinstancedetail_stateconfig.js
+++ src/app/frontend/serviceinstancedetail/serviceinstancedetail_stateconfig.js
@@ -12,93 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * @param {string} name
- * @return {?}
- */
-function getServiceInstanceDetailFakeData(name) {
-  const serviceInstanceDetailFakeData = {
-    'backend': {
-      'Status': {'Conditions': [{'Message': '', 'Reason': '', 'Status': 'True', 'Type': 'Ready'}]},
-      'apiVersion': 'catalog.k8s.io/v1alpha1',
-      'creationTimestamp': null,
-      'kind': 'ServiceInstance',
-      'metadata': {
-        'name': 'backend',
-        'namespace': 'default',
-        'selfLink': '/apis/catalog.k8s.io/v1alpha1/namespaces/default/serviceinstances/backend',
-        'uid': 'd4d4b081-e415-11e6-bf38-42010a8a0138',
-        'resourceVersion': '17160',
-        'creationTimestamp': '2017-01-26T22:21:49Z',
-      },
-      'name': 'backend',
-      'spec': {
-        'OSBCredentials': '',
-        'OSBDashboardURL': '',
-        'OSBGUID': 'b06e0534-ecc7-47c4-8d3a-5dd45c048e5b',
-        'OSBInternalID': '',
-        'OSBLastOperation': '',
-        'OSBPlanID': '0CEC1E1F-CAFA-40C7-9761-491D0B164204',
-        'OSBServiceID': '5F21BAA6-CF52-43DF-8797-3A7D3C1F5629',
-        'OSBSpaceGUID': '',
-        'OSBType': '',
-        'Parameters': null,
-        'planName': 'default',
-        'serviceClassName': 'booksbe',
-      },
-    },
-  };
-  return serviceInstanceDetailFakeData[name];
-}
-
-/**
- * @param {string} name
- * @return {?}
- */
-function getServiceBindingListFakeData(name) {
-  let serviceBindingListFakeData = {
-    'kind': 'ServiceBindingList',
-    'serviceBindings': [{
-      'Status': {'Conditions': [{'Message': '', 'Reason': '', 'Status': 'True', 'Type': 'Ready'}]},
-      'apiVersion': 'catalog.k8s.io/v1alpha1',
-      'creationTimestamp': null,
-      'kind': 'ServiceBinding',
-      'metadata': {
-        'name': 'database',
-        'namespace': 'default',
-        'selfLink': '/apis/catalog.k8s.io/v1alpha1/namespaces/default/servicebindings/database',
-        'uid': 'e9446fba-e415-11e6-bf38-42010a8a0138',
-        'resourceVersion': '17221',
-        'creationTimestamp': '2017-01-26T22:22:24Z',
-      },
-      'name': 'database',
-      'spec': {
-        'AppLabelSelector': {},
-        'ConfigMapName': '',
-        'OSBGUID': '09290646-2857-43a0-8b7e-7e8ce1bd2fef',
-        'Parameters': null,
-        'SecretName': '',
-        'instanceRef': {'name': 'backend', 'namespace': 'default'},
-        'serviceName': 'booksbe',
-      },
-    }],
-    'metadata': {
-      'selfLink': '/apis/catalog.k8s.io/v1alpha1/namespaces/default/servicebindings',
-      'resourceVersion': '958778',
-    },
-    'listMeta': {
-      'totalItems': 1,
-    },
-    'apiVersion': 'catalog.k8s.io/v1alpha1',
-  };
-  serviceBindingListFakeData.serviceBindings = serviceBindingListFakeData.serviceBindings.filter(
-      (binding) => binding.spec.instanceRef.name === name);
-  serviceBindingListFakeData.listMeta.totalItems =
-      serviceBindingListFakeData.serviceBindings.length;
-  return serviceBindingListFakeData;
-}
-
-
 import {stateName as chromeStateName} from 'chrome/chrome_state';
 import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_service';
 import {appendDetailParamsToUrl} from 'common/resource/resourcedetail';
@@ -118,7 +31,9 @@ export default function stateConfig($stateProvider) {
     url: appendDetailParamsToUrl(stateUrl),
     parent: chromeStateName,
     resolve: {
+      'serviceInstanceDetailResource': getServiceInstanceDetailResource,
       'serviceInstance': resolveServiceInstanceDetail,
+      'serviceBindingListResource': getServiceBindingListResource,
       'serviceBindingList': resolveServiceBindingList,
     },
     data: {
@@ -137,18 +52,50 @@ export default function stateConfig($stateProvider) {
   });
 }
 
+
 /**
- * @return {?}
+ * @param {!./../common/resource/resourcedetail.StateParams} $stateParams
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource}
  * @ngInject
  */
-export function resolveServiceInstanceDetail($stateParams) {
-  return Promise.resolve(getServiceInstanceDetailFakeData($stateParams.objectName));
+export function getServiceInstanceDetailResource($stateParams, $resource) {
+  return $resource(`api/v1alpha1/serviceinstance/${$stateParams.objectName}`);
 }
 
 /**
- * @return {?}
+ * @param {!angular.Resource} serviceInstanceDetailResource
+ * @return {!angular.$q.Promise}
  * @ngInject
  */
-export function resolveServiceBindingList($stateParams) {
-  return Promise.resolve(getServiceBindingListFakeData($stateParams.objectName));
+export function resolveServiceInstanceDetail(serviceInstanceDetailResource) {
+  return serviceInstanceDetailResource.get().$promise;
+}
+
+
+/**
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource}
+ * @ngInject
+ */
+export function getServiceBindingListResource($resource) {
+  return $resource(`api/v1alpha1/servicebinding`);
+}
+
+/**
+ * @param {!angular.Resource} serviceBindingListResource
+ * @param {!./../common/resource/resourcedetail.StateParams} $stateParams
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function resolveServiceBindingList(serviceBindingListResource, $stateParams) {
+  return serviceBindingListResource.get().$promise.then((serviceBindingList) => {
+    serviceBindingList.serviceBindings = serviceBindingList.items.filter(
+        (binding) => binding.spec.instanceRef.name === $stateParams.objectName);
+    delete serviceBindingList.items;
+    serviceBindingList.listMeta = {
+      totalItems: serviceBindingList.serviceBindings.length,
+    };
+    return serviceBindingList;
+  });
 }
