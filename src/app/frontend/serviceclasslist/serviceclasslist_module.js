641262f2a8988a4729a5156cee471ff9fa474ffe
Ross Holland <rossholland@rossholland.sea.corp.google.com>
2017-02-24 10:44:14 -0800

    Added icons and other metadata properties to ther service class list list view

--- src/app/frontend/serviceclasslist/serviceclasslist_module.js
+++ src/app/frontend/serviceclasslist/serviceclasslist_module.js
@@ -19,7 +19,6 @@ import {serviceClassCardComponent} from './serviceclasscard_component';
 import {serviceClassCardColumnComponent} from './serviceclasscardcolumn_component';
 import {serviceClassCardColumnMultiLineComponent} from './serviceclasscardcolumnmultiline_component';
 import {serviceClassCardColumnsComponent} from './serviceclasscardcolumns_component';
-import {serviceClassCardIconComponent} from './serviceclasscardicon_component';
 import {serviceClassCardListComponent} from './serviceclasscardlist_component';
 import {serviceClassCardListColumnHeaderComponent} from './serviceclasscardlistcolumnheader_component';
 import {serviceClassCardListColumnHeadersComponent} from './serviceclasscardlistcolumnheaders_component';
@@ -50,7 +49,6 @@ export default angular
     .component('kdServiceClassCardColumn', serviceClassCardColumnComponent)
     .component('kdServiceClassCardColumnMultiLine', serviceClassCardColumnMultiLineComponent)
     .component('kdServiceClassCardColumns', serviceClassCardColumnsComponent)
-    .component('kdServiceClassCardIcon', serviceClassCardIconComponent)
     .component('kdServiceClassCardList', serviceClassCardListComponent)
     .component('kdServiceClassCardListColumnHeader', serviceClassCardListColumnHeaderComponent)
     .component('kdServiceClassCardListColumnHeaders', serviceClassCardListColumnHeadersComponent)
